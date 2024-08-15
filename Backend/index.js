e253ec75a894325fe25ed82a50aa0ed8be5948e9
Pooja <pkatyarmal@gmail.com>
2024-08-16 00:23:04 +0530

    Add backend and frontend folders

--- Backend/index.js
+++ Backend/index.js
@@ -24,10 +24,10 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// app.post('/createUser', (req, res) => {
-//   // Handle the request here
-//   res.send('User created');
-// });
+app.post('/createUser', (req, res) => {
+  // Handle the request here
+  res.send('User created');
+});
 
 // app.listen(3000, () => {
 //   console.log('Server running on port 3000');
